e154f7c8fe41c952e4aa6c833a997c0c3dd29af1
Adan <Yaez>
2019-09-28 00:51:35 -0500

    Modificando el rest de producto para que muestre las categorias

--- routes/api-rest/productRest.js
+++ routes/api-rest/productRest.js
@@ -4,4 +4,20 @@ var keystone = require('keystone');
 
 ProductRestController.parentRestController.List = keystone.list('Product');
 
+
+ProductRestController.parentRestController.get = (request, response) => {
+	ProductRestController.parentRestController.List
+		.model
+		.findOne({
+			slug: request.params.id,
+		}).populate('categories')
+		.exec((errors, item) => {
+			if (errors) return response.json({ errors: errors });
+			if (!item) return response.json({ error: 'Not found' });
+			return response.json({
+				item: item,
+			});
+		});
+};
+
 exports.controller = ProductRestController.parentRestController;
